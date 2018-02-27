bff0912f6c4ef89cd2975fb1aab2dc1f8313c2b2
hujianbo <hujianbo>
2018-02-27 18:14:31 +0800

    add bubblesort :innocent:

---
+++ src/main/java/arithmetic/BubbleSort.java
@@ -0,0 +1,31 @@
+package arithmetic;
+
+
+/**
+ * 冒泡排序
+ * Created by hujianbo on 2018/2/27.
+ */
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] arr = {6, 3, 8, 2, 9, 1, 2};
+        System.out.println("排序前数组为：");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println();
+        System.out.println("排序后的数组为：");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
