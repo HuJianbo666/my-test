de2e7ce61d796d0f35a52c8d7cf3a2971d560dda
unclehh <18856306321@163.com>
2019-02-17 10:14:03 +0800

    static实现单例

---
+++ src/main/java/Singleton.java
@@ -0,0 +1,18 @@
+/**
+ * 单例
+ *
+ * @Author: Hu Jianbo
+ * @Date: 2019/2/17 0017 上午 10:10
+ */
+public class Singleton {
+    private Singleton() {
+    }
+
+    private static class SingletonHolder {
+        private static final Singleton INSTANCE = new Singleton();
+    }
+
+    public static Singleton getUniqueInstance() {
+        return SingletonHolder.INSTANCE;
+    }
+}
