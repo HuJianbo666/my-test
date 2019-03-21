b9798118a5019af5cc0a763f91df25b50019aa0c
HuJianbo <jbhu27@best-inc.com>
2019-03-21 12:23:26 +0800

    getDefaultThreadPool

---
+++ src/main/java/juc/ThreadPoolFactory.java
@@ -0,0 +1,46 @@
+package juc;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author HuJianbo
+ */
+public class ThreadPoolFactory {
+    private static volatile ThreadPoolExecutor pool;
+
+    public static ThreadPoolExecutor getDefaultThreadPool() {
+        if (pool == null) {
+            synchronized (ThreadPoolExecutor.class) {
+                if (pool == null) {
+                    pool = new ThreadPoolExecutor(2,
+                            3,
+                            10L,
+                            TimeUnit.SECONDS,
+                            new LinkedBlockingQueue<>(),
+                            new ThreadFactory() {
+                                final AtomicInteger threadNumber = new AtomicInteger(1);
+                                final String namePrefix = "self-defined-";
+
+                                @Override
+                                public Thread newThread(Runnable r) {
+                                    Thread t = new Thread(r, namePrefix + threadNumber.getAndIncrement());
+                                    if (t.isDaemon()) {
+                                        t.setDaemon(true);
+                                    }
+                                    if (t.getPriority() != Thread.NORM_PRIORITY) {
+                                        t.setPriority(Thread.NORM_PRIORITY);
+                                    }
+                                    return t;
+                                }
+                            });
+                }
+            }
+
+        }
+        return pool;
+    }
+}
