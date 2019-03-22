a7a4a9500b71b26c2b6589016847ed8908bc77e5
HuJianbo <jbhu27@best-inc.com>
2019-03-22 15:01:58 +0800

    CountDownLatch

---
+++ src/main/java/juc/tools/CountDownLatchTest.java
@@ -0,0 +1,55 @@
+package juc.tools;
+
+import juc.ThreadPoolFactory;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * CountDownLatch
+ *
+ * @author HuJianbo
+ */
+public class CountDownLatchTest {
+    public static void main(String[] args) {
+        CountDownLatch latch = new CountDownLatch(2);
+        ThreadPoolExecutor pool = ThreadPoolFactory.getDefaultThreadPool();
+        pool.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println(Thread.currentThread().getName() + "正在执行");
+                    TimeUnit.SECONDS.sleep(2);
+                    System.out.println(Thread.currentThread().getName() + "执行完毕");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                latch.countDown();
+            }
+        });
+
+        pool.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println(Thread.currentThread().getName() + "正在执行");
+                    TimeUnit.SECONDS.sleep(2);
+                    System.out.println(Thread.currentThread().getName() + "执行完毕");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                latch.countDown();
+            }
+        });
+
+        try {
+            System.out.println("等待两个线程执行完毕...");
+            latch.await();
+            System.out.println("两个线程已经执行完毕");
+            System.out.println("继续执行主线程");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
